beb3ea07a6ff230730a6095f845ac336207b4f36
Praveen <rpraveen.engineer@gmail.com>
2023-10-29 21:03:51 +0530

    Added player coyote timer

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -9,7 +9,8 @@ const FRICTION: int = 1000
 
 var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-@onready var animated_sprite_2d = $AnimatedSprite2D
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var coyote_jump_timer: Timer = $CoyoteJumpTimer
 
 func _process(delta: float):
 	apply_gravity(delta)
@@ -18,7 +19,11 @@ func _process(delta: float):
 	handle_movement(input_axis, delta)
 	apply_friction(input_axis, delta)
 	update_animation(input_axis)
+	var was_on_floor: bool = is_on_floor()
 	move_and_slide()
+	var just_left_ledge: bool = was_on_floor and not is_on_floor() and velocity.y >= 0
+	if just_left_ledge:
+		coyote_jump_timer.start()
 
 
 func apply_gravity(delta: float):
@@ -27,10 +32,10 @@ func apply_gravity(delta: float):
 
 
 func handle_jump():
-	if is_on_floor():
+	if is_on_floor() or coyote_jump_timer.time_left > 0.0:
 		if Input.is_action_just_pressed("jump"):
 			velocity.y = JUMP_SPEED
-	else:
+	if not is_on_floor():
 		if Input.is_action_just_released("jump") and velocity.y < JUMP_SPEED / 2.0:
 			velocity.y = JUMP_SPEED / 2.0
 
