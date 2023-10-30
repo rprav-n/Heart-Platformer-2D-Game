92dd7dfa210b008ce99e24b8280f62478068d388
Praveen <rpraveen.engineer@gmail.com>
2023-10-30 22:50:35 +0530

    Added level completed UI

--- scenes/collectible/heart.gd
+++ scenes/collectible/heart.gd
@@ -7,4 +7,4 @@ func _on_body_entered(_body: Node2D):
 	queue_free()
 	var hearts_count: int = get_tree().get_nodes_in_group("Hearts").size()
 	if hearts_count == 1:
-		print("Level complete")
+		Events.level_completed.emit()
